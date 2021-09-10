665f5f3eaf0364d5c8d2999b7c689612fd22bc7a
Nitin pandey <55994328+nitinpandey02@users.noreply.github.com>
2021-09-10 20:19:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import Home from './pages/homepage'
 import React from 'react';
 //import ReactDOM from 'react-dom';
@@ -32,8 +8,6 @@ function content() {
     <Home />
 
   );
-  
->>>>>>> Stashed changes
 }
 
 export default content;
